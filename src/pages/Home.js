46197170b85278a7fbccad17ce35225dc1d278f0
Jihee <sjh709@naver.com>
2024-01-20 19:23:09 +0900

    로딩

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,37 @@ import { movieAction } from '../redux/actions/movieAction';
 import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../components/Banner';
 import MovieSlide from '../components/MovieSlide';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { popularMovies, topRatedMovies, upcomingMovies } = useSelector(
-    (state) => state.movie
-  );
-
+  const { popularMovies, topRatedMovies, upcomingMovies, loading } =
+    useSelector((state) => state.movie);
   //   console.log('home', popularMovies);
 
   useEffect(() => {
     dispatch(movieAction.getMovies());
   }, []);
 
+  // loading이 true면 loading스피너를 보여주고
+  // loading이 false면 데이터를 보여주고
+  // true: 데이터 도착 전
+  // false: 데이터 도착 후, 에러
+  if (loading) {
+    return (
+      <ClipLoader
+        color='#E8DEFF'
+        loading={loading}
+        size={150}
+        aria-label='Loading Spinner'
+        data-testid='loader'
+      />
+    );
+  }
+
   return (
     <div>
-      {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <Banner movie={popularMovies.results[0]} />
       <h1>Popular Movie</h1>
       <MovieSlide movies={popularMovies} />
       <h1>Top rated Movie</h1>
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -2,27 +2,36 @@ import api from '../api';
 
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = api.get(`/movie/popular`);
-    const topRatedApi = api.get(`/movie/top_rated`);
-    const upComingApi = api.get(`/movie/upcoming`);
+    try {
+      // 데이터 도착 전
+      dispatch({ type: 'GET_MOVIES_REQUEST' });
 
-    let [popularMovies, topRatedMovies, upcomingMovies] = await Promise.all([
-      popularMovieApi,
-      topRatedApi,
-      upComingApi,
-    ]);
+      const popularMovieApi = api.get(`/movie/popular`);
+      const topRatedApi = api.get(`/movie/top_rated`);
+      const upComingApi = api.get(`/movie/upcoming`);
 
-    dispatch({
-      type: 'GET_MOVIES_SUCCESS',
-      payload: {
-        popularMovies: popularMovies.data,
-        topRatedMovies: topRatedMovies.data,
-        upcomingMovies: upcomingMovies.data,
-      },
-    });
-    // console.log('popularMovies', popularMovies);
-    // console.log('topRatedMovies', topRatedMovies);
-    // console.log('upcomingMovies', upcomingMovies);
+      let [popularMovies, topRatedMovies, upcomingMovies] = await Promise.all([
+        popularMovieApi,
+        topRatedApi,
+        upComingApi,
+      ]);
+      // 데이터 도착 후
+
+      dispatch({
+        type: 'GET_MOVIES_SUCCESS',
+        payload: {
+          popularMovies: popularMovies.data,
+          topRatedMovies: topRatedMovies.data,
+          upcomingMovies: upcomingMovies.data,
+        },
+      });
+      // console.log('popularMovies', popularMovies);
+      // console.log('topRatedMovies', topRatedMovies);
+      // console.log('upcomingMovies', upcomingMovies);
+    } catch (error) {
+      // 에러 핸들링
+      dispatch({ type: 'GET_MOVIES_FAILURE' });
+    }
   };
 }
 
