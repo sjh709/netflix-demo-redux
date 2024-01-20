8eeb6313c1c88ccdfa1b39f1582d5789062b3472
Jihee <sjh709@naver.com>
2024-01-20 19:48:50 +0900

    카드 디테일

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
 
 const MovieCard = ({ item }) => {
   return (
@@ -8,7 +9,20 @@ const MovieCard = ({ item }) => {
         backgroundImage: `url(https://media.themoviedb.org/t/p/w710_and_h400_multi_faces${item.poster_path})`,
       }}
     >
-      MovieCard
+      <div className='overlay'>
+        <h1>{item.title}</h1>
+        <div>
+          {item.genre_ids.map((id, index) => (
+            <Badge bg='danger' key={index}>
+              {id}
+            </Badge>
+          ))}
+        </div>
+        <div>
+          <span>{item.vote_average}</span>
+          <span>{item.adult ? '청불' : 'Under 18'}</span>
+        </div>
+      </div>
     </div>
   );
 };
